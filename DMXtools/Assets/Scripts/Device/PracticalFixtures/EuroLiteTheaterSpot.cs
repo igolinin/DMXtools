ead069d92e1a9b40e199feb9e46ff71d8116c724
igolinin <igolinin@gmail.com>
2021-04-14 13:02:05 +0200

    color picker

--- DMXtools/Assets/Scripts/Device/PracticalFixtures/EuroLiteTheaterSpot.cs
+++ DMXtools/Assets/Scripts/Device/PracticalFixtures/EuroLiteTheaterSpot.cs
@@ -7,15 +7,52 @@ namespace IA
     [ExecuteAlways]
     public class EuroLiteTheaterSpot : DMXFixture
     {
+        
         public override int getUniverse { get { return universe = 8; } }
         public override int getDmxAddress { get { return dmxAddress; } }
         public override int getNumberOfChannels { get { return numberOfChannels; } }
         public override Dictionary<string, int> getChannelFunctions { get { return channelFunctions; } }
         [SerializeField]
+        bool sendDmx;
+       
+        [SerializeField]
+        RenderTexture colorProbeTexture;
+        [SerializeField]
+        Color32 color;
+        [SerializeField]
         string[] Channels;
         [SerializeField]
         private Dictionary<string, int> channelFunctions = new Dictionary<string, int> {{ ChannelName.RED, 0 }, { ChannelName.GREEN, 1 }, { ChannelName.BLUE, 2 }, { ChannelName.WHITE, 3 },{ ChannelName.DIMMER, 4 }, { ChannelName.STROBE, 5 }, { ChannelName.SILENT_NORMAL_BOOST, 6 }  };
+        void Update()
+        {
+            ReadColor();
+            UpdateDMX();
+        }
+        void ReadColor()
+        {
+            RenderTexture tempTexture = RenderTexture.GetTemporary(colorProbeTexture.width, colorProbeTexture.height, 24);
+            Graphics.Blit(colorProbeTexture, tempTexture);
+            RenderTexture previous = RenderTexture.active;
+            RenderTexture.active = tempTexture;
 
+            Texture2D tempTexture2D = new Texture2D(colorProbeTexture.width, colorProbeTexture.height);
+            tempTexture2D.ReadPixels(new Rect(0,0, tempTexture.width, tempTexture.height), 0,0);
+            tempTexture2D.Apply();
+
+            RenderTexture.active = previous;
+            RenderTexture.ReleaseTemporary(tempTexture);
+
+            Color32[] colors = tempTexture2D.GetPixels32();
+            color = colors[0];
+            
+            //Debug.Log(colors[0].r+"  "+colors[0].g+"  "+colors[0].b+"  "+colors.Length);
+        }
+        void UpdateDMX()
+        {
+            artNetData.dmxDataMap[getUniverse - 1][getDmxAddress - 1 + getChannelFunctions[ChannelName.RED]] = (byte)color.r;
+            artNetData.dmxDataMap[getUniverse - 1][getDmxAddress - 1 + getChannelFunctions[ChannelName.GREEN]] = (byte)color.g;
+            artNetData.dmxDataMap[getUniverse - 1][getDmxAddress - 1 + getChannelFunctions[ChannelName.BLUE]] = (byte)color.b;
+        }
 
     }
     
---
+++ DMXtools/Assets/Scripts/Utils/StayInCamera.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+ 
+[ExecuteAlways]
+
+public class StayInCamera : MonoBehaviour
+{
+    Transform mainCamera;
+    [SerializeField]
+    float offset;
+
+    void OnEnable()
+    {
+        mainCamera = GameObject.FindGameObjectsWithTag("MainCamera")[0].transform;
+        transform.position = mainCamera.position + mainCamera.forward * offset;
+        transform.rotation = new Quaternion(0.0f, mainCamera.rotation.y, 0.0f, mainCamera.rotation.w);
+    }
+        void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = mainCamera.position + mainCamera.forward * offset;
+        transform.rotation = new Quaternion(0.0f, mainCamera.rotation.y, 0.0f, mainCamera.rotation.w);
+    }
+}
